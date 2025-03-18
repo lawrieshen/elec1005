2277063c9f3548fa86a5372716d7e5b52fb40509
Lawrence <lawrenceshen1213@gmail.com>
2025-03-19 10:05:35 +1100

    fix home page layout

--- community-management/src/App.tsx
+++ community-management/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Events from "./pages/Events";
 import Login from "./pages/Login";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { Layout, Menu } from "antd";
 import NavBar from "./components/NavBar";
-import { Layout } from "antd";
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -22,12 +22,17 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/events" element={user ? <Events /> : <Navigate to="/login" replace />} /> {/* Protected Route */}
-      </Routes>
+      <Layout style={{ minHeight: "100vh", width: "100vw"}}>
+        <NavBar />
+        <Layout.Content style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/events" element={user ? <Events /> : <Navigate to="/login" replace />} />
+          </Routes>
+        </Layout.Content>
+      </Layout>
     </BrowserRouter>
   );
 }
